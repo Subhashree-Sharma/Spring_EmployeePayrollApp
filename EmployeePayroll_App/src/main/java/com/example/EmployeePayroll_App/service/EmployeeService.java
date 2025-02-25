bfb8c852bfa0b254677a145cf34596136e30d257
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-02-25 15:48:55 +0530

    [Subhashree Sharma]add. Uc_01(Section-2)HandlingEmp.Payroll DTO and Model

--- EmployeePayroll_App/src/main/java/com/example/EmployeePayroll_App/service/EmployeeService.java
+++ EmployeePayroll_App/src/main/java/com/example/EmployeePayroll_App/service/EmployeeService.java
@@ -1,41 +1,54 @@
 package com.example.EmployeePayroll_App.service;
 import com.example.EmployeePayroll_App.model.Employee;
 import com.example.EmployeePayroll_App.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class EmployeeService {
 
-    //UC_02
+    //UC_01 (Section -2) Handling Employee Payroll DTO and Model
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-    private final EmployeeRepository employeeRepository;
-
-    public EmployeeService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
     }
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    public Employee addEmployee(Employee employee) {
-        return employeeRepository.save(employee);
-    }
+    //UC_02
+//    private final EmployeeRepository employeeRepository;
+//
+//    public EmployeeService(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+//
+//    public List<Employee> getAllEmployees() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    public Employee addEmployee(Employee employee) {
+//        return employeeRepository.save(employee);
+//    }
+//
+//    public Employee getEmployeeById(int id) {
+//        return employeeRepository.findById(id).orElse(null);
+//    }
+//
+//    public Employee updateEmployee(int id, Employee employee) {
+//        if (employeeRepository.existsById(id)) {
+//            employee.setId(id);
+//            return employeeRepository.save(employee);
+//        }
+//        return null;
+//    }
+//
+//    public void deleteEmployee(int id) {
+//        employeeRepository.deleteById(id);
+//    }
 
-    public Employee getEmployeeById(int id) {
-        return employeeRepository.findById(id).orElse(null);
-    }
-
-    public Employee updateEmployee(int id, Employee employee) {
-        if (employeeRepository.existsById(id)) {
-            employee.setId(id);
-            return employeeRepository.save(employee);
-        }
-        return null;
-    }
-
-    public void deleteEmployee(int id) {
-        employeeRepository.deleteById(id);
-    }
 }
